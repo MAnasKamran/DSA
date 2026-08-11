27e516e363cbb0b678436e3192f3817baaafc9c2
Anas <studanns1@gmail.com>
2026-08-12 02:47:32 +0500

    Starting out with vectors.

--- vector.cpp
+++ vector.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 int main(){
-    
+    vector<int> vec = {0, 5, 10};
+    cout << "Size: " << vec.size() << '\n';
+
+    vec.push_back(15);
+    vec.push_back(20);
+    vec.push_back(25);
+
+    for(int v : vec){
+        cout << v << '\n';
+    }
+
+    vec.pop_back();
+    cout << '\n';
+
+    for(int v : vec){
+        cout << v << '\n';
+    }
+
+    cout << "First value: " << vec.front() << '\n';
+    cout << "Last value: " << vec.back() << '\n';
+
+    cout << vec.at(2);
     return 0;
 }
