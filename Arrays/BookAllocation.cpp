c4e16efeb3be86899facae030f25bf71174f2653
Anas <studanns1@gmail.com>
2026-09-01 20:16:54 +0500

    BookAllocation

---
+++ Arrays/BookAllocation.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isValid(vector<int> &arr, int n, int m, int mid){
+    int student = 1, pages = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > mid){
+            return false;
+        }
+        if(pages + arr[i] <= mid){
+            pages += arr[i];
+        }
+        else{
+            student++;
+            pages = arr[i];
+        }
+    }
+    return (student > m) ? false : true;
+}
+int allocateBooks(vector<int> &arr, int n, int m){
+    if(m > n){
+        return -1;
+    }
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    int st = 0, end = sum, ans = -1;
+
+    while(st <= end){
+        int mid = st + (end - st) / 2;
+
+        if(isValid(arr, n, m, mid)){
+            ans = mid;
+            end = mid - 1;
+        }
+        else{
+            st = mid + 1;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr = {2, 1, 3, 4};
+    int n = 4, m = 2;
+    cout << allocateBooks(arr, n, m) << '\n';
+    return 0;
+}
